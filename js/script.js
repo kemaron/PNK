f9d53b53ffef49c931939ff3335d0651cd475a7a
Daniel Balcer <kemaron@gmail.com>
2019-02-08 17:08:40 +0100

    Add function computerMove

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 var buttonKamien = document.getElementById ('kamien');
 var buttonPapier = document.getElementById ('papier');
 var buttonNozyce = document.getElementById ('nozyce');
@@ -12,13 +11,28 @@ buttonNozyce.addEventListener ('click',playerMove);
 
 function playerMove () {
     var userMove = this.id;
-    console.log (userMove);
-    showScore (userMove);
-    
-};
+    computerMove();
+    //showScore (userMove);    
+    showScore ('tacos','pupos');     
+}
 
-function showScore (rezultat) {
+function computerMove () {
+    // losuje wynik ruchu komputera zgodnie ze wzorem:  Math.floor(Math.random()*(max-min+1)+min);
+    var computerHand =  Math.floor(Math.random()*(3-1+1)+1);
+    if (computerHand === 1) {
+        computerHand = 'papier';
+    }
+    else if (computerHand === 2) {
+        computerHand = 'kamien';
+    }
+    else {
+        computerHand = 'nozyce';
+    }
+    return computerHand;
+}
+
+function showScore (rezultat,rez2) {
     var tablicaWynikow = document.getElementById ('scoreBoard');
-    tablicaWynikow.innerHTML += rezultat+' <br>';
+    tablicaWynikow.innerHTML += rezultat+' <br>'+ rez2;
 }
 
