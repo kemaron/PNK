60749496af5810c14cbd6e3e02f122b08374581c
Daniel Balcer <kemaron@gmail.com>
2019-02-12 17:15:40 +0100

    Fix mentor's remarks

--- js/script.js
+++ js/script.js
@@ -1,46 +1,48 @@
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable prefer-template */
+'use strict';
 
 
-const buttonNewGame = document.getElementById('newGame');
-const buttonKamien = document.getElementById('kamien');
-const buttonPapier = document.getElementById('papier');
-const buttonNozyce = document.getElementById('nozyce');
-const tablicaWynikow = document.getElementById('scoreBoard');
-let startGame = false;
-const result = document.getElementById('result');
-let humanSum = 0;
-let computerSum = 0;
-let ileGier;
+var buttonNewGame = document.getElementById('newGame');
+var buttonStone = document.getElementById('kamien');
+var buttonPaper = document.getElementById('papier');
+var buttonScissors = document.getElementById('nozyce');
+var scoreBoard = document.getElementById('scoreBoard');
+var startGame = false;
+var result = document.getElementById('result');
+var humanSum = 0;
+var computerSum = 0;
+var howManyGames;
 
 buttonNewGame.addEventListener('click', newGame);
-buttonKamien.addEventListener('click', playerMove);
-buttonPapier.addEventListener('click', playerMove);
-buttonNozyce.addEventListener('click', playerMove);
+buttonStone.addEventListener('click', playerMove);
+buttonPaper.addEventListener('click', playerMove);
+buttonScissors.addEventListener('click', playerMove);
 
-// console.dir (buttonNewGame);
-
-function newGame() {
+function newGame () {
   // zbezpieczam przed wpisaniem wartości z poza zakresu ilości gier (1-10), lub wciśnięciem Anuluj
-  ileGier = window.prompt('Ile wygranych rund kończy grę?');
-  if ((isNaN(ileGier) === true) || ileGier === null || ileGier <= 0 || ileGier >= 11) {
-    tablicaWynikow.innerHTML = 'Podaj liczbę z zakresu 1-10';
-  } else {
+  howManyGames = window.prompt('Ile wygranych rund kończy grę?');
+  if ((isNaN(howManyGames) === true) || howManyGames === null || howManyGames <= 0 || howManyGames >= 11) {
+    scoreBoard.innerHTML = 'Podaj liczbę z zakresu 1-10';
+  } 
+  else {
     startGame = true;
-    buttonNewGame.innerHTML = `Do ${ileGier} wygranych`;
+    buttonNewGame.innerHTML = ('Do ' + [howManyGames] + ' wygranych');
     // czyszcze tablice z rezultatami
     result.innerHTML = '';
-    tablicaWynikow.innerHTML = '';
+    scoreBoard.innerHTML = '';
     // czyszcze wyniki sumaryczne
     humanSum = 0;
     computerSum = 0;
   }
 }
 
-function playerMove() {
+function playerMove () {
   // sprawdzam, czy mogę zacząć nową grę - zmienna startGame
   if (startGame === true) {
-    const userChoice = this.id;
-    const computerChoice = computerMove();
-    let theWinner;
+    var userChoice = this.id;
+    var computerChoice = computerMove();
+    var theWinner;
     // sprawdzam remis
     if (userChoice === computerChoice) {
       theWinner = 'remis';
@@ -49,42 +51,51 @@ function playerMove() {
     else if ((userChoice === 'kamien' && computerChoice === 'nozyce') || (userChoice === 'papier' && computerChoice === 'kamien') || (userChoice === 'nozyce' && computerChoice === 'papier')) {
       theWinner = 'human';
       humanSum++;
-    } else {
+    } 
+    else {
       theWinner = 'computer';
       computerSum++;
     }
     showScore(userChoice, computerChoice, theWinner);
-  } else {
-    tablicaWynikow.innerHTML = 'Proszę wcisnąć "Nowa Gra", aby rozpocząć rozgrywkę';
+  } 
+  else {
+    scoreBoard.innerHTML = 'Proszę wcisnąć "Nowa Gra", aby rozpocząć rozgrywkę';
   }
 }
 
-function computerMove() {
+function computerMove () {
   // losuje wynik ruchu komputera zgodnie ze wzorem:  Math.floor(Math.random()*(max-min+1)+min);
-  let computerHand = Math.floor(Math.random() * (3 - 1 + 1) + 1);
+  var computerHand = Math.floor(Math.random() * 3 + 1);
   if (computerHand === 1) {
     computerHand = 'papier';
-  } else if (computerHand === 2) {
+  } 
+  else if (computerHand === 2) {
     computerHand = 'kamien';
-  } else {
+  } 
+  else {
     computerHand = 'nozyce';
   }
   return computerHand;
 }
 
-function showScore(player, computer, win) {
+function showScore (player, computer, win) {
   if (win === 'remis') {
-    tablicaWynikow.innerHTML = 'REMIS <br>';
-  } else if (win === 'human') {
-    tablicaWynikow.innerHTML = `WYGRAŁEŚ! Twój wybór to ${player} ,a komputera ${computer}<br>`;
-  } else {
-    tablicaWynikow.innerHTML = `PRZEGRAŁEŚ :-( Twój wybór to ${player} ,a AI ${computer}<br>`;
+    scoreBoard.innerHTML = 'REMIS <br>';
+  } 
+  else if (win === 'human') {
+    scoreBoard.innerHTML = ('WYGRAŁEŚ! Twój wybór to ' + [player] + ' ,a komputera ' + [computer] + '<br>');
+  } 
+  else {
+    scoreBoard.innerHTML = ('PRZEGRAŁEŚ :-( Twój wybór to ' + [player] + ', a AI ' + [computer] + '<br>');
   }
-  result.innerHTML = `${humanSum}-${computerSum}`;
+  result.innerHTML = ([humanSum] + '-' + [computerSum]);
+  endGame();
+}
 
-  // sprawdzam, czy osiągnięto wystarczającą ilość wygranych rund
-  if (humanSum == ileGier || computerSum == ileGier) {
-    tablicaWynikow.innerHTML += '<BR>GAME OVER';
+// sprawdzam, czy osiągnięto wystarczającą ilość wygranych rund
+function endGame () {
+  if (humanSum == howManyGames || computerSum == howManyGames) {
+    scoreBoard.innerHTML += '<BR>GAME OVER';
     startGame = false;
     buttonNewGame.innerHTML = 'Nowa gra';
   }
